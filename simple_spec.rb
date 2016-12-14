3b252f6ba23056c37c875a1e96f60ea7c67a71d2
Винокурова Мария Владимировна <mvinokurova@at-consulting.ru>
2016-12-14 15:55:08 +0300

    #sel2 "Инфраструктура для работы с Selenium" #task 1

---
+++ simple_spec.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+require 'selenium-webdriver'
+
+describe 'Amazon search ' do
+  before(:each) do
+    @driver = Selenium::WebDriver.for(:chrome)
+    @wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+  end
+  it 'should should find rspec book' do
+    test_url = 'https://www.amazon.com'
+    query_string = 'The Rspec Book'
+    expected_title = "Amazon.com: #{query_string}"
+
+    @driver.navigate.to test_url
+    @driver.find_element(:id, 'twotabsearchtextbox').send_keys(query_string)
+    @driver.find_element(:css, "input[value='Go']").click
+    puts @driver.title
+    @wait.until{@driver.title == expected_title}
+  end
+  after(:each) do
+    @driver.quit
+  end
+end
